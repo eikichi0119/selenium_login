97be58faed1e0e8f42e26109c53466ba84761164
eikichi0119 <eikichi0119@gmail.com>
2019-03-20 03:21:33 -0400

    Changed using a file to get parameters.

--- login.py
+++ login.py
@@ -2,23 +2,30 @@ import time
 import sys
 from selenium import webdriver
 
-if len(sys.argv) < 2:
-    print 'Missed argument needful. Please try "python login.py fully_server_URL account password"'
-    sys.exit()
+# Use arguments to get parameters.
+#if len(sys.argv) < 2:
+#    print 'Missed argument needful. Please try "python login.py fully_server_URL account password"'
+#    sys.exit()
 
-testurl = sys.argv[1]
-account = sys.argv[2]
-password = sys.argv[3]
+#testurl = sys.argv[1]
+#account = sys.argv[2]
+#password = sys.argv[3]
+
+with open("parameters.txt", "r") as ins:
+    array = []
+    for line in ins:
+        array.append(line.rstrip('\n'))
 
 
 driver = webdriver.Chrome()
-driver.get(testurl)
+#driver = webdriver.Firefox()
+driver.get(array[0])
 
 element = driver.find_element_by_name("user");
-element.send_keys(account);
+element.send_keys(array[1]);
 
 element = driver.find_element_by_name("pass");
-element.send_keys(password);
+element.send_keys(array[2]);
 
 submit_button = driver.find_elements_by_xpath('/html/body/table/tbody/tr/td/table/tbody/tr/td[2]/table/tbody/tr[2]/td/table[1]/tbody/tr[4]/td[3]/input')[0]
 
